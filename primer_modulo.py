d6564bea14a3d9118d70a067c4ecedbc10d49846
JohnCard <107288337+JohnCard@users.noreply.github.com>
2022-12-07 23:46:34 -0600

    11:46 p. m.

---
+++ primer_modulo.py
@@ -0,0 +1,7 @@
+print('Hola mundo, y hola quetal querido usuario. ')
+nombre = input('Por favor digite su nombre: ')
+apellido = input('Digite también su apellido por favor: ')
+edad = int(input('Digite su edad: '))
+
+print('Sea cordialmente bienvenido',nombre+' '+apellido)
+print('Su edad es de aproximadamente: '+str(edad))
